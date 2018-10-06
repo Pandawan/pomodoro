4581094cb1f6a436a2c273f383a06c7a6ce54979
PandawanFr <pandawanfr@gmail.com>
2018-10-05 17:28:51 -0700

    Updates

---
+++ precache-manifest.0b9006ab859fadeb4e1339e32d1ae7c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a702e67ff471c6ecfc33",
+    "url": "/pomodoro/static/js/runtime~main.a702e67f.js"
+  },
+  {
+    "revision": "d627a9e9461872ace09e",
+    "url": "/pomodoro/static/js/main.d627a9e9.chunk.js"
+  },
+  {
+    "revision": "863ada2a80d20e74397e",
+    "url": "/pomodoro/static/js/1.863ada2a.chunk.js"
+  },
+  {
+    "revision": "d627a9e9461872ace09e",
+    "url": "/pomodoro/static/css/main.7927cef1.chunk.css"
+  },
+  {
+    "revision": "863ada2a80d20e74397e",
+    "url": "/pomodoro/static/css/1.16999cda.chunk.css"
+  },
+  {
+    "revision": "75efe7eaea67c8a40d0393ff4ab2e82e",
+    "url": "/pomodoro/index.html"
+  }
+];
